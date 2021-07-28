c108238e212b34661c2c1f36d8add3df46fb3cef
HariniMansani <42859566+harinimansani@users.noreply.github.com>
2021-07-28 12:29:12 +0530

    Add files via upload

---
+++ objectss.js
@@ -0,0 +1,9 @@
+//object
+console.log("hello")
+let student={
+    name:"bhavana"
+}
+console.log(student.name)
+// accessing properties
+const stud={name:"harini",lastname:"mansani",age:"21"}
+console.log("hi my name is "+stud.name+" "+stud.lastname+" and i am "+stud.age+" years old")
