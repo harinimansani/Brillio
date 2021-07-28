84ec883c5a43ec335736d98328484bc91ed285b6
HariniMansani <42859566+harinimansani@users.noreply.github.com>
2021-07-28 12:22:32 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,15 @@
+//string example
+var h=()=>{return"hello"}
+console.log(h())
+//addition example
+var add=(a,b)=>{ return a+b}
+console.log(add(2,3))
+//another way
+var mul=(a,b)=> a*b
+console.log(mul(2,3))
+//another example
+var name;
+var hi=(name)=>{
+    return "hello"+name
+}
+console.log(hi(' harini'));
