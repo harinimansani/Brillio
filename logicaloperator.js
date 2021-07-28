a33e3f43c08ea55891cbbda7d75465e122a886e3
HariniMansani <42859566+harinimansani@users.noreply.github.com>
2021-07-28 12:21:44 +0530

    Add files via upload

---
+++ logicaloperator.js
@@ -0,0 +1,17 @@
+age=10;
+
+//and
+if(age>18 && age<65)
+{
+    console.log("You are eligible to vote for elections")
+}
+//or
+if(age<18 || age<65)
+{
+    console.log("You are not eligible to vote for elections")
+}
+//not
+if(!0)
+{
+    console.log("after use of operator its value is TRUE")
+}
---
+++ specialoperators.js
@@ -0,0 +1,10 @@
+//turnery operator
+var x
+5>2?x=1:x=0;
+console.log(x)
+//, operator
+array=[1,2,3]
+array[3]=4
+console.log(array)
+//typeof operator
+console.log(typeof(x))
